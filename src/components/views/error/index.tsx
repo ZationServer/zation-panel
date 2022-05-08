dd4c90490b507f01a7efc44b8fb8b9852a9e3445
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2022-05-08 22:32:01 +0200

    Implement error view.

---
+++ src/components/views/error/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './index.css';
+import {Fab} from "@mui/material";
+import {Refresh, Warning} from '@mui/icons-material';
+import {ROOT_PANEL_PATH} from "../../../lib/utils/constants";
+import Center from "../../utils/center";
+
+const Error: React.FC<{message: string}> = ({message}) => {
+    const reload = () => window.location.pathname = ROOT_PANEL_PATH;
+    return (
+        <Center className={"full-height"}>
+            <div className={'errorContainer'}>
+                <Warning className="warningIcon bounceInDown animated"/>
+                <h1>{message}</h1>
+                <Fab variant="extended" aria-label="Delete" onClick={reload} className="refreshButton">
+                    <Refresh className="refreshIcon" />
+                    Try again
+                </Fab>
+            </div>
+        </Center>
+    );
+};
+
+export default Error;
