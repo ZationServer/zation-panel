340dce6021047a2f577e351abe35a6148cfc7638
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2022-05-08 22:38:32 +0200

    Implement LatencyStats component.

---
+++ src/components/lib/stats/placeableStats/latencyStats/index.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import useConnector from "../../../../../lib/hooks/useConnector";
+import RTLineChartCard from "../../core/rtLineChartCard";
+
+const LatencyStats: React.FC<{
+    interval?: number,
+    maxLength?: number,
+    mini?: boolean
+}> = ({interval = 1000,maxLength = 10,mini}) => {
+    const connector = useConnector();
+    useEffect(() => {
+        return connector.requireLatencyUpdates(interval);
+    },[connector,interval]);
+    return <RTLineChartCard
+        unit={"ms"}
+        description="Latency"
+        maxLength={maxLength}
+        fetchValue={() => connector.lastLatency}
+        interval={interval}
+        grace={10}
+        mini={mini}
+        suggestedMax={10}
+        valueInfoTooltip={"Only to the currently connected worker"}
+    />
+};
+
+export default LatencyStats;
