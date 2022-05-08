b8b1b5fbd4a35d23a49667e4ec5d7cbff848c506
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2022-05-08 22:31:50 +0200

    Implement loading view.

---
+++ src/components/views/loading/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {ScaleLoader} from 'react-spinners';
+import Center from "../../utils/center";
+
+export const Loading = () => {
+    return (
+        <Center className={"full-height fadeIn animated"}>
+            <ScaleLoader
+                height={"7em"}
+                width={"0.6em"}
+                color={'var(--primary-color)'}
+            />
+        </Center>
+    );
+};
