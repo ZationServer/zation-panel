3e35912a336fb0d1ccb50e9ed7e7ef4fe80d295c
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2022-05-08 03:36:33 +0200

    Implement root index.

---
+++ src/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import './lib/loader/chartjsLoader';
+
+import ReactDOM from 'react-dom';
+import './index.scss';
+import './assets/libs/animate.css';
+import './assets/libs/all.min.css';
+import App from './app';
+import reportWebVitals from './reportWebVitals';
+import {ClientContext} from "./lib/hooks/useClient";
+import {Client} from "zation-client";
+import {APIDefinition} from "./lib/definitions/apiDefinition";
+import {DEV, SERVER_PATH} from "./lib/utils/constants";
+import {ConnectorContext} from "./lib/hooks/useConnector";
+import Connector from "./lib/core/connector";
+
+ReactDOM.render(
+  <React.StrictMode>
+      <ClientContext.Provider value={new Client<APIDefinition>(!DEV ? {
+          hostname: window.location.hostname,
+          port:  parseInt(window.location.port),
+          path: SERVER_PATH,
+          secure: (window.location.protocol === 'https:'),
+          rejectUnauthorized: false,
+          debug: false
+      } : {port: 4001,debug: true},true)}>
+          <ConnectorContext.Provider value={new Connector()}>
+              <App/>
+          </ConnectorContext.Provider>
+      </ClientContext.Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
