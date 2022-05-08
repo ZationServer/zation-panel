cbce12e1c17edb4dffe339c4fa033237072bb485
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2022-05-08 22:38:19 +0200

    Implement ExternalClientDistributionStats component.

---
+++ src/components/lib/stats/placeableStats/externalClientDistribution/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import useConnector from "../../../../../lib/hooks/useConnector";
+import RTPercentageBarCard from "../../core/rtPercentageBarCard";
+import RTLineChartCard from "../../core/rtLineChartCard";
+
+const ExternalClientDistributionStats: React.FC<{
+    interval?: number,
+    bar?: boolean,
+    mini?: boolean,
+    maxLength?: number,
+}> = ({interval = 1000,maxLength = 10,bar,mini}) => {
+    const connector = useConnector();
+    if(!bar) return <RTLineChartCard
+        description="Clients distribution"
+        maxLength={maxLength}
+        fetchValue={() => connector.workersSummary.clientDistribution}
+        interval={interval}
+        grace={0}
+        mini={mini}
+        valueInfoTooltip={"Standard deviation of clients across workers"}
+    />
+    else return <RTPercentageBarCard
+        description={"Clients distribution"}
+        fetchPercent={() => connector.workersSummary.clientDistribution}
+        interval={interval}
+    />
+};
+
+export default ExternalClientDistributionStats;
---
+++ src/components/lib/stats/placeableStats/externalClientsCountStats/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import useConnector from "../../../../../lib/hooks/useConnector";
+import RTLineChartCard from "../../core/rtLineChartCard";
+
+const ExternalClientsCountStats: React.FC<{
+    interval?: number,
+    maxLength?: number,
+    mini?: boolean
+}> = ({interval = 1000,maxLength = 10,mini}) => {
+    const processLabel = (value: number) => (value > 1 || value === 0) ? "clients connected" : "client connected";
+    const connector = useConnector();
+    return <RTLineChartCard
+        dataLabel={processLabel}
+        description={processLabel}
+        maxLength={maxLength}
+        fetchValue={() => connector.workersSummary.clientCount}
+        interval={interval}
+        grace={10}
+        mini={mini}
+    />
+};
+
+export default ExternalClientsCountStats;
