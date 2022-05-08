d70523ee18b975883ab260f5399d1f0177bc85ff
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2022-05-08 22:30:46 +0200

    Implement math utils.

---
+++ src/lib/utils/math.ts
@@ -0,0 +1,6 @@
+export function calcStandardDeviation(array: number[]): number {
+    if(array.length === 0) return 0;
+    const n = array.length
+    const mean = array.reduce((a, b) => a + b) / n
+    return Math.sqrt(array.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n)
+}
