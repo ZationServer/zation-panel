1a17ea49580f4135f1e96538e386d2afd5e12cad
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2022-05-08 03:24:37 +0200

    Implement path constants.

---
+++ src/lib/utils/constants.ts
@@ -0,0 +1,8 @@
+export const DEV = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+
+const PATH = window.location.pathname;
+const parsedRootPanelPath = (PATH.substring(0,(PATH.lastIndexOf('/panel')+6))
+    .replace(/(\/)+$/,'')) || "/";
+export const ROOT_PANEL_PATH = parsedRootPanelPath === '' ? '/' : parsedRootPanelPath;
+
+export const SERVER_PATH = PATH.substring(0,PATH.indexOf("/panel")) || "/";
