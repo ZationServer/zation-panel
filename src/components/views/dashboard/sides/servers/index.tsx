78310cd565abb6dca4e815aa15d99677561f5c93
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2022-05-08 22:41:33 +0200

    Implement dashboard Servers page component.

---
+++ src/components/views/dashboard/sides/servers/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Grid} from "@mui/material";
+import MemoryUsageStats from "../../../../lib/stats/placeableStats/memoryUsageStats";
+import CpuUsageStats from "../../../../lib/stats/placeableStats/cpuUsageStats";
+
+const Servers: React.FC = () => {
+
+    return (
+        <div className="container-fluid">
+            <div className="animated fadeIn">
+                <Grid container columnSpacing={{ xs: 1, sm: 2, md: 3 }} rowSpacing={{ xs: 3, sm: 3, md: 3 }}>
+                    <Grid item xs={12} sm={6} md={6}>
+                        <CpuUsageStats/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={6}>
+                        <MemoryUsageStats/>
+                    </Grid>
+                </Grid>
+            </div>
+        </div>
+    )
+};
+
+export default React.memo(Servers);
