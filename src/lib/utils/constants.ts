0e318c77df3ea5c01084961cfeb82b51256ac097
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2022-05-17 00:14:10 +0200

    Improve ROOT_PANEL_PATH works when /panel is not in the URL path.

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -7,8 +7,9 @@ Copyright(c) Ing. Luca Gian Scaringella
 export const DEV = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 
 const PATH = window.location.pathname;
-const parsedRootPanelPath = (PATH.substring(0,(PATH.lastIndexOf('/panel')+6))
+const panelIndex = PATH.lastIndexOf('/panel');
+const parsedRootPanelPath = (PATH.substring(0,(panelIndex+6))
     .replace(/(\/)+$/,'')) || "/";
-export const ROOT_PANEL_PATH = parsedRootPanelPath === '' ? '/' : parsedRootPanelPath;
+export const ROOT_PANEL_PATH = (panelIndex === -1 || parsedRootPanelPath === '') ? '/' : parsedRootPanelPath;
 
 export const SERVER_PATH = PATH.substring(0,PATH.indexOf("/panel")) || "/";
