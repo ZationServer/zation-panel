d334772007a3fed5a3f7b2f4890cce81f2894440
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2022-05-27 02:41:11 +0200

    Add AddServerRecommendation alerts to the servers side.

--- src/components/views/dashboard/sides/servers/index.tsx
+++ src/components/views/dashboard/sides/servers/index.tsx
@@ -6,9 +6,10 @@ Copyright(c) Ing. Luca Gian Scaringella
 
 import React from 'react';
 import {Grid} from "@mui/material";
-import MemoryUsageStats from "../../../../lib/stats/placeableStats/memoryUsageStats";
-import CpuUsageStats from "../../../../lib/stats/placeableStats/cpuUsageStats";
-import ServersTable from "../../../../lib/stats/placeableStats/serversTable";
+import MemoryUsageStats from "../../../../lib/stats/placeableStats/cluster/memoryUsageStats";
+import CpuUsageStats from "../../../../lib/stats/placeableStats/cluster/cpuUsageStats";
+import ServersTable from "../../../../lib/stats/placeableStats/cluster/serversTable";
+import AddServerRecommendationAlert from "../../../../lib/alerts/AddServerRecommendationAlert";
 
 const Servers: React.FC = () => {
 
@@ -16,6 +17,8 @@ const Servers: React.FC = () => {
         <div className="container-fluid">
             <div className="animated fadeIn">
                 <Grid container columnSpacing={{ xs: 1, sm: 2, md: 3 }} rowSpacing={{ xs: 3, sm: 3, md: 3 }}>
+                    <AddServerRecommendationAlert type={'broker'} workloadMark={10}/>
+                    <AddServerRecommendationAlert type={'worker'}/>
                     <Grid item xs={12} sm={6} md={6}>
                         <CpuUsageStats/>
                     </Grid>
