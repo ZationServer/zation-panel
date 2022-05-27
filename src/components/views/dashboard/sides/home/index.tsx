7b13fe1ec639b48b824241e33a7ce6660cad92e6
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2022-05-27 02:39:24 +0200

    Update Home side.

--- src/components/views/dashboard/sides/home/index.tsx
+++ src/components/views/dashboard/sides/home/index.tsx
@@ -6,12 +6,13 @@ Copyright(c) Ing. Luca Gian Scaringella
 
 import React from 'react';
 import {Grid} from "@mui/material";
-import ExternalClientsCountStats from "../../../../lib/stats/placeableStats/externalClientsCountStats";
-import OnlineWorkersCountStats from "../../../../lib/stats/placeableStats/onlineServersCountStats";
-import MessageCountStats from "../../../../lib/stats/placeableStats/messageCountStats";
-import NetworkStatus from "../../../../lib/stats/placeableStats/networkStatus";
-import ResourceUsageStats from "../../../../lib/stats/placeableStats/resourceUsageStats";
-import LatencyStats from "../../../../lib/stats/placeableStats/latencyStats";
+import ExternalClientsCountStats from "../../../../lib/stats/placeableStats/cluster/externalClientsCountStats";
+import OnlineWorkersCountStats from "../../../../lib/stats/placeableStats/cluster/onlineServersCountStats";
+import MessageCountStats from "../../../../lib/stats/placeableStats/cluster/messageCountStats";
+import NetworkStatus from "../../../../lib/stats/placeableStats/cluster/networkStatus";
+import WorkloadStats from "../../../../lib/stats/placeableStats/cluster/workloadStats";
+import LatencyStats from "../../../../lib/stats/placeableStats/cluster/latencyStats";
+import NotFullyLicensedAlert from "../../../../lib/alerts/NotFullyLicensedAlert";
 
 const Home: React.FC = () => {
 
@@ -19,6 +20,7 @@ const Home: React.FC = () => {
         <div className="container-fluid">
             <div className="animated fadeIn">
                 <Grid container columnSpacing={{ xs: 1, sm: 2, md: 3 }} rowSpacing={{ xs: 3, sm: 3, md: 3 }}>
+                    <NotFullyLicensedAlert/>
                     <Grid item xs={12} sm={6} md={3}>
                         <ExternalClientsCountStats mini/>
                     </Grid>
@@ -26,7 +28,7 @@ const Home: React.FC = () => {
                         <OnlineWorkersCountStats mini/>
                     </Grid>
                     <Grid item xs={12} sm={6} md={3}>
-                        <ResourceUsageStats bar/>
+                        <WorkloadStats bar/>
                     </Grid>
                     <Grid item xs={12} sm={6} md={3}>
                         <LatencyStats mini/>
